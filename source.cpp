458022b08aa37138d5dc8a34d432ecf176bb6825
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2024-03-27 16:26:14 +0900

    lsb to msb

--- source.cpp
+++ source.cpp
@@ -2,9 +2,9 @@
 int main() {
 	for (int i = 0;
 		putchar(
-			[](this auto self, bool lsb, auto... bits) {
-				if constexpr (sizeof...(bits))return (lsb << sizeof...(bits)) + self(bits...);
-				else return lsb;
+			[](this auto self, bool msb, auto... bits) {
+				if constexpr (sizeof...(bits))return (msb << sizeof...(bits)) + self(bits...);
+				else return msb;
 			}(i == 4, (3 > i) * i, !((i + 1) % 4), (i != 4) * i, i & (5 > i)) + i * 2 + 97
 				) - 115
 		; ++i);
