a921e2b08fd7a37a27eccd81d4b1be9902fe54d0
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2024-03-27 15:37:04 +0900

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,12 @@
+﻿import <iostream>;
+int main() {
+	for (int i = 0;
+		putchar(
+			[](this auto self, bool lsb, auto... bits) {
+				if constexpr (sizeof...(bits))return (lsb << sizeof...(bits)) + self(bits...);
+				else return lsb;
+			}(i == 4, (3 > i) * i, !((i + 1) % 4), (i != 4) * i, i & (5 > i)) + i * 2 + 97
+				) - 115
+		; ++i);
+	return 0;
+}
